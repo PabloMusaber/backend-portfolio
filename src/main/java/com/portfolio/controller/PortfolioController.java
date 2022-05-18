6992955749f6715b76702cdeed2bd7c0650f0acc
Pablo <musaberpablo@gmail.com>
2022-05-18 12:16:13 -0300

    Agrega seguridad en las operaciones create, update y delete a través de la autorización via JSON Web Token. La operación read es accesible para todos, no requiere ser usuario ni administrador.

--- src/main/java/com/portfolio/controller/PortfolioController.java
+++ src/main/java/com/portfolio/controller/PortfolioController.java
@@ -5,6 +5,7 @@ import com.portfolio.model.Portfolio;
 import com.portfolio.service.IPortfolioService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,25 +21,28 @@ public class PortfolioController {
     @Autowired
     private IPortfolioService portServ;
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/new/portfolio")
     public void agregarPersona (@RequestBody Portfolio port){
        portServ.crearPortfolio(port);
     }
-    
+
     @GetMapping("/ver/portfolio")
     @ResponseBody
     public List<Portfolio> verPortfolio(){
         return portServ.verPortfolio();
     }
     
-    @DeleteMapping("/delete/portfolio/{id}")
-    public void borrarPortfolio(@PathVariable Long id){
-        portServ.borrarPortfolio(id);
-    }
-    
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/editar/portfolio")
     public Portfolio editarPortfolio (@RequestBody Portfolio port){
         return portServ.editarPortfolio(port);
+    }
+    
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/delete/portfolio/{id}")
+    public void borrarPortfolio(@PathVariable Long id){
+        portServ.borrarPortfolio(id);
     }
     
 }
